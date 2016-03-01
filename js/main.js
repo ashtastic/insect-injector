e99b9b9c4a198c366c61b3b37cac62f44c7aa490
ashtastic <amurr20@gmail.com>
2016-03-01 16:30:18 -0500

    Organized all the information in the js file.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,51 @@
 // Create your variable here, name it `insects`
 
+var insects = [{
+    name: 'Ant Nest Beetle',
+    image: 'ant-nest-beetle.jpg',
+    family: 'Carabidae',
+    minSize: '6',
+    maxSize: '20',
+    eatsInsects: 'Yes',
+    eatsDung: 'No',
+    eatsPlants: 'No'
+
+  }, {
+
+    name: 'Dung Beetle',
+    image: 'dung-beetle.jpg',
+    family: 'Scarabidae',
+    minSize: '20',
+    maxSize: '30',
+    eatsInsects: 'No',
+    eatsDung: 'Yes',
+    eatsPlants: 'No'
+
+  }, {
+
+    name: 'Stag Beetle',
+    image: 'stag-beetle.jpg',
+    family: 'Lucanidae',
+    minSize: '5',
+    maxSize: '120',
+    eatsInsects: 'No',
+    eatsDung: 'No',
+    eatsPlants: 'Yes'
+
+  }, {
+
+    name: 'Tortoise Beetle',
+    image: 'tortoise-beetle.jpg',
+    family: 'Cassidinae',
+    minSize: '1',
+    maxSize: '18',
+    eatsInsects: 'No',
+    eatsDung: 'No',
+    eatsPlants: 'Yes'
+
+  }
+
+}];
 
 // ##########################################################
 // START DO NOT CHANGE ZONE
